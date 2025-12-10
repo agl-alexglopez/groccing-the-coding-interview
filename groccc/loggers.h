765cc08dbd8cf5008b4492e06a2a68847bef3e90
Alexander Lopez <agl.alexglopez@gmail.com>
2025-12-09 20:20:41 -0800

    fix logging color

--- groccc/loggers.h
+++ groccc/loggers.h
@@ -15,10 +15,10 @@
 #define logerr(format_string...) (void)fprintf(stderr, format_string);
 
 #define logfail(test_cases_name)                                               \
-    (void)fprintf(stderr, "fail for test: %s, file: %s, line: %d\n",           \
+    (void)fprintf(stderr, "\n%sFailed \"%s\" in %s on line %d.%s\n", LOG_RED,  \
                   TCG_test_case_name(test_cases_name),                         \
                   TCG_test_case_file(test_cases_name),                         \
-                  TCG_test_case_line(test_cases_name))
+                  TCG_test_case_line(test_cases_name), LOG_NONE)
 
 #define logstats(test_cases_name, passed_count_int)                            \
     (void)fprintf(stdout, "%s passed %d/%lu\n", #test_cases_name,              \
