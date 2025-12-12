817731bbe4f09769171470d336fd9949f7747598
Alexander Lopez <agl.alexglopez@gmail.com>
2025-12-12 10:47:21 -0800

    more anagram tests

--- groccc/group_anagrams/group_anagrams_tests.h
+++ groccc/group_anagrams/group_anagrams_tests.h
@@ -66,6 +66,145 @@ TCG_test_case("3 groups", {
         )
     },
 })
+TCG_test_case("all identical", {
+    .input = {
+        .strs = CCC_buffer_with_compound_literal(
+            5,
+            (SV_Str_view[5]){
+                SV_from("aa"),
+                SV_from("aa"),
+                SV_from("aa"),
+                SV_from("aa"),
+                SV_from("aa"),
+            }
+        ),
+    },
+    .output = {
+        .groups = CCC_buffer_with_compound_literal(
+            1,
+            (CCC_Buffer[1]){
+                CCC_buffer_with_compound_literal(
+                    5,
+                    (SV_Str_view[5]){
+                        SV_from("aa"),
+                        SV_from("aa"),
+                        SV_from("aa"),
+                        SV_from("aa"),
+                        SV_from("aa"),
+                    }
+                ),
+            }
+        ),
+    },
+})
+TCG_test_case("no anagrams", {
+    .input = {
+        .strs = CCC_buffer_with_compound_literal(
+            3,
+            (SV_Str_view[3]){
+                SV_from("abc"),
+                SV_from("def"),
+                SV_from("ghi"),
+            }
+        ),
+    },
+    .output = {
+        .groups = CCC_buffer_with_compound_literal(
+            3,
+            (CCC_Buffer[3]){
+                CCC_buffer_with_compound_literal(
+                    1, (SV_Str_view[1]){ SV_from("abc") }
+                ),
+                CCC_buffer_with_compound_literal(
+                    1, (SV_Str_view[1]){ SV_from("def") }
+                ),
+                CCC_buffer_with_compound_literal(
+                    1, (SV_Str_view[1]){ SV_from("ghi") }
+                ),
+            }
+        )
+    },
+})
+TCG_test_case("empty input", {
+    .input = {
+        .strs = CCC_buffer_with_compound_literal(
+            0,
+            (SV_Str_view[0]){}
+        ),
+    },
+    .output = {
+        .groups = CCC_buffer_with_compound_literal(
+            0,
+            (CCC_Buffer[0]){}
+        ),
+    },
+})
+TCG_test_case("single string", {
+    .input = {
+        .strs = CCC_buffer_with_compound_literal(
+            1,
+            (SV_Str_view[1]){
+                SV_from("solo"),
+            }
+        ),
+    },
+    .output = {
+        .groups = CCC_buffer_with_compound_literal(
+            1,
+            (CCC_Buffer[1]){
+                CCC_buffer_with_compound_literal(
+                    1,
+                    (SV_Str_view[1]){ SV_from("solo") }
+                ),
+            }
+        ),
+    },
+})
+TCG_test_case("mixed length anagrams", {
+    .input = {
+        .strs = CCC_buffer_with_compound_literal(
+            7,
+            (SV_Str_view[7]){
+                SV_from("abcd"),
+                SV_from("bcad"),
+                SV_from("dabc"),
+                SV_from("abc"),
+                SV_from("cab"),
+                SV_from("bac"),
+                SV_from("zzz")
+            }
+        ),
+    },
+    .output = {
+        .groups = CCC_buffer_with_compound_literal(
+            3,
+            (CCC_Buffer[3]){
+                CCC_buffer_with_compound_literal(
+                    3,
+                    (SV_Str_view[3]){
+                        SV_from("abcd"),
+                        SV_from("bcad"),
+                        SV_from("dabc"),
+                    }
+                ),
+                CCC_buffer_with_compound_literal(
+                    3,
+                    (SV_Str_view[3]){
+                        SV_from("abc"),
+                        SV_from("bac"),
+                        SV_from("cab"),
+                    }
+                ),
+                CCC_buffer_with_compound_literal(
+                    1,
+                    (SV_Str_view[1]){
+                        SV_from("zzz"),
+                    }
+                ),
+            }
+        ),
+    },
+})
 TCG_tests_end(group_anagrams_tests);
 
 #endif /* GROUP_ANAGRAMS_TESTS_H */
