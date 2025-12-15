d5d799428e1381c6a83373ab149c5a744ae0fc21
Alexander Lopez <agl.alexglopez@gmail.com>
2025-12-15 09:09:29 -0800

    valid sudoku

---
+++ groccc/valid_sudoku/valid_sudoku.c
@@ -0,0 +1,90 @@
+#include <stdbool.h>
+
+#define BITSET_USING_NAMESPACE_CCC
+#include "ccc/bitset.h"
+#include "ccc/types.h"
+
+#include "utility/loggers.h"
+#include "utility/test_case_generator.h"
+
+#include "valid_sudoku_tests.h"
+
+static inline bool
+is_valid_box(int const board[const 9][9], Bitset *const row_check,
+             Bitset *const col_check, size_t const row_start,
+             size_t const col_start)
+{
+    Bitset box_check = bitset_initialize(bitset_blocks(9), NULL, NULL, 9);
+    for (size_t row = row_start; row < row_start + 3; ++row)
+    {
+        for (size_t col = col_start; col < col_start + 3; ++col)
+        {
+            int const digit = board[row][col] - 1;
+            if (digit < 0)
+            {
+                continue;
+            }
+            CCC_Tribool was_on = bitset_set(&box_check, digit, CCC_TRUE);
+            if (was_on)
+            {
+                return false;
+            }
+            was_on = bitset_set(row_check, (row * 9) + digit, CCC_TRUE);
+            if (was_on)
+            {
+                return false;
+            }
+            was_on = bitset_set(col_check, (col * 9) + digit, CCC_TRUE);
+            if (was_on)
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+static struct Valid_sudoku_output
+valid_sudoku(struct Valid_sudoku_input *input)
+{
+    Bitset row_check
+        = bitset_initialize(bitset_blocks(9UL * 9UL), NULL, NULL, 9UL * 9UL);
+    Bitset col_check
+        = bitset_initialize(bitset_blocks(9UL * 9UL), NULL, NULL, 9UL * 9UL);
+    for (size_t row = 0; row < 9; row += 3)
+    {
+        for (size_t col = 0; col < 9; col += 3)
+        {
+            if (!is_valid_box(input->board, &row_check, &col_check, row, col))
+            {
+                return (struct Valid_sudoku_output){
+                    .is_valid = false,
+                };
+            }
+        }
+    }
+    return (struct Valid_sudoku_output){
+        .is_valid = true,
+    };
+}
+
+int
+main(void)
+{
+    int passed = 0;
+    TCG_for_each_test_case(valid_sudoku_tests, {
+        struct Valid_sudoku_output const output
+            = valid_sudoku(&TCG_test_case_input(valid_sudoku_tests));
+        struct Valid_sudoku_output const *const correct_output
+            = &TCG_test_case_output(valid_sudoku_tests);
+        if (output.is_valid != correct_output->is_valid)
+        {
+            logfail(valid_sudoku_tests);
+        }
+        else
+        {
+            ++passed;
+        }
+    });
+    return TCG_tests_status(valid_sudoku_tests, passed);
+}
---
+++ groccc/valid_sudoku/valid_sudoku_tests.h
@@ -0,0 +1,63 @@
+#ifndef VALID_SUDOKU_TESTS_H
+#define VALID_SUDOKU_TESTS_H
+
+#include <stdbool.h>
+
+#include "utility/test_case_generator.h"
+
+struct Valid_sudoku_input
+{
+    int board[9][9];
+};
+
+struct Valid_sudoku_output
+{
+    bool is_valid;
+};
+
+TCG_tests_begin(valid_sudoku_tests,
+                struct Valid_sudoku_input,
+                struct Valid_sudoku_output)
+TCG_test_case("valid board", {
+    .input = {
+        .board = {
+            {5,3,0, 0,7,0, 0,0,0},
+            {6,0,0, 1,9,5, 0,0,0},
+            {0,9,8, 0,0,0, 0,6,0},
+
+            {8,0,0, 0,6,0, 0,0,3},
+            {4,0,0, 8,0,3, 0,0,1},
+            {7,0,0, 0,2,0, 0,0,6},
+
+            {0,6,0, 0,0,0, 2,8,0},
+            {0,0,0, 4,1,9, 0,0,5},
+            {0,0,0, 0,8,0, 0,7,9},
+        },
+    },
+    .output = {
+        .is_valid = true,
+    },
+})
+TCG_test_case("invalid box", {
+    .input = {
+        .board = {
+            {5,3,0, 0,7,0, 0,0,0},
+            {6,0,0, 1,9,5, 0,0,0},
+            {0,9,5, 0,0,0, 0,6,0}, /* Two 5's in same box. */
+
+            {8,0,0, 0,6,0, 0,0,3},
+            {4,0,0, 8,0,3, 0,0,1},
+            {7,0,0, 0,2,0, 0,0,6},
+
+            {0,6,0, 0,0,0, 2,8,0},
+            {0,0,0, 4,1,9, 0,0,5},
+            {0,0,0, 0,8,0, 0,7,9},
+        },
+    },
+    .output = {
+        .is_valid = false,
+    },
+})
+TCG_tests_end(valid_sudoku_tests);
+
+#endif /* VALID_SUDOKU_TESTS_H */
